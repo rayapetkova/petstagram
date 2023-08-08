4279406cc0470c66b245601adcbc07138f6a5d6d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-08-08 17:57:30 +0300

    Added urls and 404 html

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from petstagram.accounts import views
 
 urlpatterns = (
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('profile/<int:pk>/', include(
-        (path('', views.show_profile_details, name='profile-details'),
-         path('edit/', views.edit_profile, name='edit-profile'),
-         path('delete/', views.delete_profile, name='delete-profile')
-         ))),
+    path('register/', views.register_page, name='register-user'),
+    path('login/', views.login_page, name='login-user'),
+    path('profile/<int:pk>/', include([
+         path('', views.profile_details_page, name='profile-details'),
+         path('edit/', views.profile_edit_page, name='edit-profile'),
+         path('delete/', views.profile_delete_page, name='delete-profile')
+         ])),
 )
--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = (
-    path('add/', views.add_photo, name='add-photo'),
-    path('<int:pk>/', include((
+    path('add/', views.photo_add_page, name='add-photo'),
+    path('<int:pk>/', include([
         path('', views.photo_details_page, name='photo-details'),
-        path('edit/', views.edit_photo, name='edit-photo')
-        ))),
+        path('edit/', views.photo_edit_page, name='edit-photo')
+        ])),
 )
