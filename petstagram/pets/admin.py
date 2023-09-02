58ba5862baf8eeb1d1178c71ca80aa08d940a237
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-02 18:36:18 +0300

    Update admin page for 'pets'

--- petstagram/pets/admin.py
+++ petstagram/pets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from petstagram.pets.models import Pet
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    pass
--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False
+    )
+
+    personal_photo = models.URLField(
+        null=False,
+        blank=False
+    )
+
+    date_of_birth = models.DateField(
+        null=False,
+        blank=True
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=True
+    )
