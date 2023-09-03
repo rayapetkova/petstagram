c15d312c908de739ead801be8973dfa8a1a81050
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-03 22:21:43 +0300

    Models for photos (need to do migrations)

--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(
+        null=False,
+        blank=False
+    )
+
+    description = models.CharField(
+        max_length=300,
+        validators=(
+            MinLengthValidator(10),
+        ),
+        null=False,
+        blank=False
+    )
+
+    location = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False
+    )
+
+    date_of_publication = models.DateField(
+        auto_now=True,
+        null=False,
+        blank=True
+    )
