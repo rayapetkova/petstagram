c3d59f1c32b119ec242fe4052c42bc0a7b7eb0e8
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-05 16:03:12 +0300

    Update common models and made migration

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from petstagram.photos.models import Photo
+
+
+class Comment(models.Model):
+    comment_text = models.CharField(max_length=300)
+    date_and_time_of_publication = models.DateTimeField(auto_now_add=True)
+    relation_to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    relation_to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
