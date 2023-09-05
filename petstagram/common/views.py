90add3047f0d86df82107b1837765c6b40567b0c
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-05 20:00:51 +0300

    Update view and home-page template

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 from petstagram.photos.models import Photo
 
 
+def find_photo_likes_count(curr_photo):
+    curr_photo.likes_count = curr_photo.like_set.count()
+    return curr_photo
+
+
 def home_page(request):
-    all_photos = Photo.objects.all()
+    all_photos = [find_photo_likes_count(photo) for photo in Photo.objects.all()]
+    for c_photo in all_photos:
+        c_photo.is_liked = True if c_photo.likes_count > 0 else False
 
     context = {
         'photos': all_photos
