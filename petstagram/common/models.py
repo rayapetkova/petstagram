4f19d7cae709527ffca223ac7e9fab478c797f9b
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-05 20:00:28 +0300

    Update models and made migration

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -4,10 +4,32 @@ from petstagram.photos.models import Photo
 
 
 class Comment(models.Model):
-    comment_text = models.CharField(max_length=300)
-    date_and_time_of_publication = models.DateTimeField(auto_now_add=True)
-    relation_to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    comment_text = models.CharField(
+        max_length=300,
+        null=True,
+        blank=True
+    )
+
+    date_and_time_of_publication = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        blank=True
+    )
+
+    relation_to_photo = models.ForeignKey(
+        Photo,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=True
+    )
 
 
 class Like(models.Model):
-    relation_to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    relation_to_photo = models.ForeignKey(
+        Photo,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=True
+    )
+
+
