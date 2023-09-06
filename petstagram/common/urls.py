73b7853066db479e72a85f4e8613e89da90704d3
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-06 22:11:57 +0300

    Added url for like functionality

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -4,4 +4,5 @@ from petstagram.common import views
 
 urlpatterns = (
     path('', views.home_page, name='home-page'),
+    path('like_photo/<int:photo_id>', views.like_photo_func, name='like-photo'),
 )
