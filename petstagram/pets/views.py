76bf9e40c77e159d4e87a6d277e9f3cc35a9d539
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-07 22:50:33 +0300

    Update pet details page

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+from petstagram.photos.models import Photo
+
+
 # Create your views here.
 def pet_add_page(request):
     return render(request, f"pets/pet-add-page.html")
@@ -10,7 +14,15 @@ def pet_delete_page(request, username, pet_slug):
 
 
 def pet_details_page(request, username, pet_slug):
-    return render(request, f"pets/pet-details-page.html")
+    curr_pet = Pet.objects.get(slug=pet_slug)
+    all_photos = curr_pet.photo_set.all()
+
+    context = {
+        'curr_pet': curr_pet,
+        'curr_pet_photos': all_photos
+    }
+
+    return render(request, f"pets/pet-details-page.html", context=context)
 
 
 def pet_edit_page(request, username, pet_slug):
